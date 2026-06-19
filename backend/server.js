23fdb558932336b795e4c617b363a262eb98ca66
yash <vtu20642@vtu.edu.in>
2026-06-19 22:16:20 +0530

    Refined CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // React Vite app dev server URL
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json());
